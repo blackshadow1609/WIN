2e0ce2ef8a649a7e4aa63a35ac5a9ad77712f47c
Sky_Time87 <blackshadow1609@yandex.ru>
2025-07-30 00:52:37 +0300

    Dialog in progress

--- WinAPI_2/main.cpp
+++ WinAPI_2/main.cpp
@@ -1,6 +1,33 @@
-#include<Windows.h>
+﻿#include<Windows.h>
 
-INT WINAPI WinMain(HINSTANCE hInstasce, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow)
+//#define MESSAGE_BOX
+
+// https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-messagebox
+//Документация для изучения
+
+INT WINAPI WinMain(HINSTANCE hInstasce, HINSTANCE hPrevInst, LPSTR lpCmdLine, INT nCmdShow) //обязательная основа
 {
 
-}
+#ifdef MESSAGE_BOX
+	MessageBox
+	(
+		NULL,
+		"Привет WinAPI\n Это самое простое окно - окно сообщения\t\t\t\t(MessageBox)",  //текст сообщения в окне
+		"Привет!",																		//навние в заголовке окна
+		MB_ABORTRETRYIGNORE | MB_ICONINFORMATION | MB_HELP								//Кнопки
+		| MB_DEFBUTTON3																	//установка кнопки по умолчанию
+		| MB_TOPMOST																	//Расположение окна (модальность)
+	);
+
+	/* MB_ - MessageBox
+	 Венгерская нотация (Hungarian notation) — соглашение об
+	 именовании переменных, констант и прочих идентификаторов
+	 в коде программ. Суть — в использовании префиксов, которые
+	 обозначают тип данных или назначение идентификатора. */
+#endif // MESSAGE_BOX
+
+
+	return 0;
+} 
+
+
