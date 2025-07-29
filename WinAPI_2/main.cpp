62a4c7bee5fbc1ba175a6b6e37668b560e4a8dbe
Sky_Time87 <blackshadow1609@yandex.ru>
2025-07-30 02:47:03 +0300

    DialogBox closing DONE

--- WinAPI_2/main.cpp
+++ WinAPI_2/main.cpp
@@ -1,5 +1,7 @@
 ﻿#include<Windows.h>
+#include"resource.h"
 
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam); //прототип функции процедуры окна
 //#define MESSAGE_BOX
 
 // https://learn.microsoft.com/en-us/windows/win32/api/winuser/nf-winuser-messagebox
@@ -26,8 +28,35 @@ INT WINAPI WinMain(HINSTANCE hInstasce, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 	 обозначают тип данных или назначение идентификатора. */
 #endif // MESSAGE_BOX
 
+	DialogBoxParam(hInstasce, MAKEINTRESOURCE(IDD_DIALOG1), NULL, (DLGPROC)DlgProc, 0);
+									 //если есть ошибка прописать (DLGPROC)DlgProc.
 
 	return 0;
-} 
+}
 
+BOOL CALLBACK DlgProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)				//реализация
+{
+	switch (uMsg)
+	{
+	case WM_INITDIALOG:						//выполняется один раз - при запуске окна
+		break;
+
+	case WM_COMMAND:						//обрабатывает нажатие кнопок, перемещения мыши и т.д.
+		switch (LOWORD(wParam))
+		{
+		case IDOK:
+			MessageBox(hwnd, "Была нажата кнопка 'ОК'", "Info", MB_OK | MB_ICONINFORMATION);
+			break;
+		case IDCANCEL:
+			EndDialog(hwnd, 0);
+			break;
+		}
+		break;
+
+	case WM_CLOSE:							//отрабатывает при нажатии на кнопку "Закрыть Х"
+		EndDialog(hwnd, 0);					
+		break;
+	}
+	return FALSE;
+}
 
