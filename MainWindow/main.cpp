1418187298c955d753feb0e2782eb6583cf951be
Sky_Time87 <blackshadow1609@yandex.ru>
2025-08-01 02:21:33 +0300

    MainWindowHW DONE

--- MainWindow/main.cpp
+++ MainWindow/main.cpp
@@ -1,7 +1,7 @@
 ﻿#include<Windows.h>
+#include<stdio.h>
 
-CONST CHAR g_sz_CLASS_NAME[] = "My First Window";	//Абсолютно у любого класса окна есть имя.
-													//Имя класса окна - это самая обычная строка.
+CONST CHAR g_sz_CLASS_NAME[] = "My First Window";
 
 INT WINAPI WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
@@ -13,12 +13,12 @@ INT WINAPI WinMain(HINSTANCE hInstasce, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 	ZeroMemory(&wClass, sizeof(wClass));
 
 	wClass.style = 0;
-	wClass.cbSize = sizeof(wClass);						//cb - Count Bytes
+	wClass.cbSize = sizeof(wClass);
 	wClass.cbWndExtra = 0;
 	wClass.cbClsExtra = 0;
 
 	wClass.hIcon = LoadIcon(NULL, IDI_APPLICATION);
-	wClass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);	//Sm - Small
+	wClass.hIconSm = LoadIcon(NULL, IDI_APPLICATION);
 	wClass.hCursor = LoadCursor(NULL, IDC_ARROW);
 	wClass.hbrBackground = (HBRUSH)COLOR_WINDOW;
 
@@ -34,19 +34,30 @@ INT WINAPI WinMain(HINSTANCE hInstasce, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 	}
 
 	// 2) Создание окна:
-	
+
+	int screenWidth = GetSystemMetrics(SM_CXSCREEN);
+	int screenHeight = GetSystemMetrics(SM_CYSCREEN);
+
+	int windowWidth = screenWidth * 0.75;
+	int windowHeight = screenHeight * 0.75;
+
+	int windowX = (screenWidth - windowWidth) / 2;
+	int windowY = (screenHeight - windowHeight) / 2;
+
+	char windowTitle[256];
+	sprintf_s(windowTitle, "Size: %dx%d, Position: %d,%d", windowWidth, windowHeight, windowX, windowY);
+
 	HWND hwnd = CreateWindowEx
 	(
-		NULL,							//ExStyle
-		g_sz_CLASS_NAME,				//ClassName
-		g_sz_CLASS_NAME,				//WindowName (Title) - эта строка отображается в заголовке окна
-		WS_OVERLAPPEDWINDOW,			//Такой стиль задается для всех главных окон. 
-										//Это окно будет родительским для других окон приложения.
-		CW_USEDEFAULT, CW_USEDEFAULT,	//Position. Позиция окна
-		CW_USEDEFAULT, CW_USEDEFAULT,	//Size. Размер окна
-		NULL,							//ParentWindow  
-		NULL,							//Строка меню для главного окна, или же ID-ресурса для дочернего окна
-		hInstasce,						//Это экземпляр *.ехе - файла нашей программы 
+		NULL,
+		g_sz_CLASS_NAME,
+		windowTitle,								
+		WS_OVERLAPPEDWINDOW,
+		windowX, windowY,				
+		windowWidth, windowHeight,	
+		NULL,
+		NULL,
+		hInstasce,
 		NULL
 	);
 	if (hwnd == NULL)
@@ -54,18 +65,18 @@ INT WINAPI WinMain(HINSTANCE hInstasce, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 		MessageBox(NULL, "Window creation failed", "", MB_OK | MB_ICONERROR);
 		return 0;
 	}
-	ShowWindow(hwnd, nCmdShow);			//Задает режим отображения окна: развернуто/свернуто/свернуто на панель задач
-	UpdateWindow(hwnd);					//Прорисовка рабочей обрасти окна
-	
+	ShowWindow(hwnd, nCmdShow);
+	UpdateWindow(hwnd);
+
 	// 3) Запуск цикла сообщений:
-	 
+
 	MSG msg;
 	while (GetMessage(&msg, NULL, 0, 0) > 0)
 	{
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
-							
+
 	return msg.message;
 }
 
