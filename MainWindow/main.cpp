874add6eca68f771190ec64d24431a31dbfbfcb7
Sky_Time87 <blackshadow1609@yandex.ru>
2025-08-01 05:54:08 +0300

    Window Size & Position DONE

--- MainWindow/main.cpp
+++ MainWindow/main.cpp
@@ -1,4 +1,5 @@
 ﻿#include<Windows.h>
+#include<stdio.h>
 
 CONST CHAR g_sz_CLASS_NAME[] = "My First Window";	//Абсолютно у любого класса окна есть имя.
 													//Имя класса окна - это самая обычная строка.
@@ -34,6 +35,15 @@ INT WINAPI WinMain(HINSTANCE hInstasce, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 	}
 
 	// 2) Создание окна:
+
+	INT screen_width = GetSystemMetrics(SM_CXSCREEN);
+	INT screen_height = GetSystemMetrics(SM_CYSCREEN);
+
+	INT window_width = screen_width * 0.75;
+	INT window_height = screen_height * 3 / 4;
+
+	INT window_start_x = screen_width / 8;
+	INT window_start_y = screen_height / 8;			
 	
 	HWND hwnd = CreateWindowEx
 	(
@@ -42,8 +52,8 @@ INT WINAPI WinMain(HINSTANCE hInstasce, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 		g_sz_CLASS_NAME,				//WindowName (Title) - эта строка отображается в заголовке окна
 		WS_OVERLAPPEDWINDOW,			//Такой стиль задается для всех главных окон. 
 										//Это окно будет родительским для других окон приложения.
-		CW_USEDEFAULT, CW_USEDEFAULT,	//Position. Позиция окна
-		CW_USEDEFAULT, CW_USEDEFAULT,	//Size. Размер окна
+		window_start_x, window_start_y,	//Position. Позиция окна
+		window_width, window_height,	//Size. Размер окна
 		NULL,							//ParentWindow  
 		NULL,							//Строка меню для главного окна, или же ID-ресурса для дочернего окна
 		hInstasce,						//Это экземпляр *.ехе - файла нашей программы 
@@ -75,14 +85,40 @@ INT WINAPI WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 	case WM_CREATE:
 		break;
+
+	case WM_MOVE:
+	case WM_SIZE:
+	{
+		RECT window_rect;					//Rectangle - Прямоугольник
+		GetWindowRect(hwnd, &window_rect);
+		INT window_width = window_rect.right - window_rect.left;
+		INT window_height = window_rect.bottom - window_rect.top;
+		CONST INT SIZE = 256;
+		CHAR sz_title[SIZE] = {};
+		sprintf
+		(
+			sz_title, 
+			"%s - Position: %i x %i, Size: %i x %i", 
+			g_sz_CLASS_NAME, 
+			window_rect.left, window_rect.top,
+			window_width, window_height
+		);
+
+		SendMessage(hwnd, WM_SETTEXT, 0, (LPARAM)sz_title);
+	}
+		break;
+
 	case WM_COMMAND:
 		break;
+
 	case WM_DESTROY:
 		PostQuitMessage(0);
 		break;
+
 	case WM_CLOSE:
 		DestroyWindow(hwnd);
 		break;
+
 	default:
 		return DefWindowProc(hwnd, uMsg, wParam, lParam);
 	}
