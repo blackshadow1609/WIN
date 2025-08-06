8cd7be975aaf42b6f53a7adf76e3a0b99cc5394c
Sky_Time87 <blackshadow1609@yandex.ru>
2025-08-06 09:08:00 +0300

    ButtonsHW DONE

--- Calc/main.cpp
+++ Calc/main.cpp
@@ -49,7 +49,7 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 		NULL
 	);
 	ShowWindow(hwnd, nCmdShow);
-	UpdateWindow(hwnd);						// taskkill /f /im calc.exe - способ завершить работу приложения через CMD
+	UpdateWindow(hwnd);						
 	
 	//3)Запуск цикла сообщений
 
@@ -65,32 +65,73 @@ INT WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInst, LPSTR lpCmdLine, IN
 
 INT WINAPI WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
-	switch (uMsg)
-	{
-	case WM_CREATE:
-	{
-		HWND hEditDisplay = CreateWindowEx
-		(
-			NULL, "Edit", "0",
-			WS_CHILD | WS_VISIBLE | WS_BORDER | ES_RIGHT,
-			10, 10,
-			500, 22,
-			hwnd,
-			(HMENU)IDC_EDIT_DISPLAY,
-			GetModuleHandle(NULL),
-			NULL
-			);
-	}
-		break;
-	case WM_COMMAND:
-		break;
-	case WM_DESTROY:
-		PostQuitMessage(0);
-		break;
-	case WM_CLOSE:
-		DestroyWindow(hwnd);
-		break;
-	default:return DefWindowProc(hwnd, uMsg, wParam, lParam);
-	}
-	return FALSE;
+    switch (uMsg)
+    {
+    case WM_CREATE:
+    {
+        HWND hEditDisplay = CreateWindowEx(
+            NULL, "Edit", "0",
+            WS_CHILD | WS_VISIBLE | WS_BORDER | ES_RIGHT,
+            10, 10,
+            230, 30,
+            hwnd,
+            (HMENU)IDC_EDIT_DISPLAY,
+            GetModuleHandle(NULL),
+            NULL
+        );
+
+        CreateWindow("BUTTON", "7", WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+            10, 50, 50, 30, hwnd, (HMENU)IDC_BUTTON_7, NULL, NULL);
+        CreateWindow("BUTTON", "8", WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+            70, 50, 50, 30, hwnd, (HMENU)IDC_BUTTON_8, NULL, NULL);
+        CreateWindow("BUTTON", "9", WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+            130, 50, 50, 30, hwnd, (HMENU)IDC_BUTTON_9, NULL, NULL);
+        CreateWindow("BUTTON", "+", WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+            190, 50, 50, 30, hwnd, (HMENU)IDC_BUTTON_PLUS, NULL, NULL);
+
+        CreateWindow("BUTTON", "4", WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+            10, 90, 50, 30, hwnd, (HMENU)IDC_BUTTON_4, NULL, NULL);
+        CreateWindow("BUTTON", "5", WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+            70, 90, 50, 30, hwnd, (HMENU)IDC_BUTTON_5, NULL, NULL);
+        CreateWindow("BUTTON", "6", WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+            130, 90, 50, 30, hwnd, (HMENU)IDC_BUTTON_6, NULL, NULL);
+        CreateWindow("BUTTON", "-", WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+            190, 90, 50, 30, hwnd, (HMENU)IDC_BUTTON_MINUS, NULL, NULL);
+
+        CreateWindow("BUTTON", "1", WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+            10, 130, 50, 30, hwnd, (HMENU)IDC_BUTTON_1, NULL, NULL);
+        CreateWindow("BUTTON", "2", WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+            70, 130, 50, 30, hwnd, (HMENU)IDC_BUTTON_2, NULL, NULL);
+        CreateWindow("BUTTON", "3", WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+            130, 130, 50, 30, hwnd, (HMENU)IDC_BUTTON_3, NULL, NULL);
+        CreateWindow("BUTTON", "*", WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+            190, 130, 50, 30, hwnd, (HMENU)IDC_BUTTON_ASTER, NULL, NULL);
+
+        CreateWindow("BUTTON", "0", WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+            10, 170, 50, 30, hwnd, (HMENU)IDC_BUTTON_0, NULL, NULL);
+        CreateWindow("BUTTON", ".", WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+            70, 170, 50, 30, hwnd, (HMENU)IDC_BUTTON_POINT, NULL, NULL);
+        CreateWindow("BUTTON", "=", WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+            130, 170, 50, 30, hwnd, (HMENU)IDC_BUTTON_EQUAL, NULL, NULL);
+        CreateWindow("BUTTON", "/", WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+            190, 170, 50, 30, hwnd, (HMENU)IDC_BUTTON_SLASH, NULL, NULL);
+
+        CreateWindow("BUTTON", "BSP", WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+            10, 210, 110, 30, hwnd, (HMENU)IDC_BUTTON_BSP, NULL, NULL);
+        CreateWindow("BUTTON", "CLR", WS_VISIBLE | WS_CHILD | BS_PUSHBUTTON,
+            130, 210, 110, 30, hwnd, (HMENU)IDC_BUTTON_CLR, NULL, NULL);
+    }
+    break;
+    case WM_COMMAND:
+        break;
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    case WM_CLOSE:
+        DestroyWindow(hwnd);
+        break;
+    default:
+        return DefWindowProc(hwnd, uMsg, wParam, lParam);
+    }
+    return FALSE;
 }
