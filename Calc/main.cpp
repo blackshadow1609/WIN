b27a088df3f3b44ccbef28657e61227eee6b5414
Sky_Time87 <blackshadow1609@yandex.ru>
2025-08-08 00:23:32 +0300

    Calculations in progress

--- Calc/main.cpp
+++ Calc/main.cpp
@@ -1,5 +1,6 @@
 ﻿#include<Windows.h>
 #include<float.h>
+#include<stdio.h>
 #include"resource.h"
 
 CONST CHAR g_sz_CLASS_NAME[] = "MyCalc";
@@ -92,9 +93,9 @@ INT WINAPI WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 {
 	static DOUBLE a = DBL_MIN;
 	static DOUBLE b = DBL_MIN;
-	static CHAR operation = 0;
+	static INT operation = 0;
 	static BOOL input = FALSE;
-	static BOOL input_operation = FALSE;
+	static BOOL input_operation = FALSE;							
 
 	switch (uMsg)
 	{
@@ -202,8 +203,8 @@ INT WINAPI WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		HWND hEditDisplay = GetDlgItem(hwnd, IDC_EDIT_DISPLAY);
 		if (LOWORD(wParam) >= IDC_BUTTON_0 && LOWORD(wParam) <= IDC_BUTTON_POINT)
 		{
-			if (LOWORD(wParam) == IDC_BUTTON_POINT)
-				szDigit[0] = '.';
+			if (input == FALSE) SendMessage (hEditDisplay, WM_SETTEXT, 0, (LPARAM)"0");
+			if (LOWORD(wParam) == IDC_BUTTON_POINT) szDigit[0] = '.';
 			else 
 				szDigit[0] = LOWORD(wParam) - IDC_BUTTON_0 + '0';
 			SendMessage(hEditDisplay, WM_GETTEXT, g_SIZE, (LPARAM)szDisplay);
@@ -216,19 +217,19 @@ INT WINAPI WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 		if (LOWORD(wParam) >= IDC_BUTTON_PLUS && LOWORD(wParam) <= IDC_BUTTON_SLASH)
 		{
 			SendMessage(hEditDisplay, WM_GETTEXT, g_SIZE, (LPARAM)szDisplay);
-			//if (input && a == DBL_MIN) a = atof(szDisplay);
-			//else b = atof(szDisplay);
-			(input && a == DBL_MIN ? a : b) = atof(szDisplay);
+			if (input && a == DBL_MIN) a = atof(szDisplay);
+			if (input) b = atof(szDisplay);
+			//(input && a == DBL_MIN ? a : b) = atof(szDisplay);
 			input = FALSE;
 			SendMessage(hwnd, WM_COMMAND, (WPARAM)operation, 0);
-			operation = wParam;
+			operation = LOWORD(wParam);
 			input_operation = TRUE;
 		}
 		if (LOWORD(wParam) >= IDC_BUTTON_EQUAL)
 		{
 			SendMessage(hEditDisplay, WM_GETTEXT, g_SIZE, (LPARAM)szDisplay);
 			//if (input && a == DBL_MIN) a = atof(szDisplay);
-			//else b = atof(szDisplay);
+			if (input) b = atof(szDisplay);
 			(input && a == DBL_MIN ? a : b) = atof(szDisplay);
 			input = FALSE;
 			switch (operation)
@@ -237,8 +238,11 @@ INT WINAPI WndProc(HWND hwnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 			case IDC_BUTTON_MINUS:	a -= b;	break;
 			case IDC_BUTTON_ASTER:	a *= b;	break;
 			case IDC_BUTTON_SLASH:	a /= b;	break;
+			//default: MessageBox(hwnd, "");
 			}
 			input_operation = FALSE;
+			sprintf(szDisplay, "%g", a);
+			SendMessage(hEditDisplay, WM_SETTEXT, 0, (LPARAM)szDisplay);
 		}
 	}
 		break;
